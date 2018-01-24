bd377b860be85c3ce371e978beb668d4c5c8a1d3
PJ623 <jardiolin.paolo@gmail.com>
2018-01-23 17:42:33 -0800

    set up routing

--- test-server.js
+++ test-server.js
@@ -4,15 +4,32 @@ var http = require('http');
 var fs = require('fs');
 
 let myServer = http.createServer((req, res) => {
-    let body = '';
-    fs.readdir('.', (err, list) => {
-        list.forEach((a) => {
-            body += a + '\n';
-            //console.log(body);
-            return body;
-        });
-        res.end(body);
-    });
+    switch (req.url) {
+        case '/':
+            res.writeHead(200, { 'Content-Type': 'text/plain' });
+            res.end("Hello world!");
+            break;
+        case '/ls':
+            res.writeHead(200, { 'Content-Type': 'text/plain' });
+            getDirItems((result) => res.end(result));
+            break;
+        default:
+            res.writeHead(404, { 'Content-Type': 'text/plain' });
+            res.end('Page not found.');
+            break;
+    }
 });
 
-myServer.listen(3000);
+function getDirItems(callback){
+    let body = '';
+    fs.readdir('.', (err, list) => {
+        for(let i = 0; i < list.length; i++)
+            body += list[i] + '\n';
+
+        callback(body);
+    });
+}
+
+const port = 3000;
+myServer.listen(port);
+console.log("Server started on port", port.toString() + '.', "Press CTRL+C to stop.");
