b8a06125a9b2490a35155ac40e073fd3ebc59995
PJ623 <jardiolin.paolo@gmail.com>
2018-01-25 15:29:57 -0800

    did styling and modularized code

--- server.js
+++ server.js
@@ -8,19 +8,35 @@ var fs = require('fs');
 var myServer = http.createServer((req, res) => {
 
     // Generic function that sends files.
-    function fetchFile(dir, callback) {
-        fs.readFile(dir, (err, data) => {
-            callback(data.toString());
-        });
+    function fetchFile(dir) {
+        // Check file extension.
+        if (dir.match(/.html$/)) {
+            fs.readFile(dir, (err, data) => {
+                sendPage(data.toString());
+            });
+        } else if(dir.match(/.css$/)){
+            fs.readFile(dir, (err, data) => {
+                sendCSS(data.toString());
+            });
+        } else if(dir.match(/.js$/)){
+            console.log('JS needs to be sent.');
+        }
     }
 
-    // Puts the view and the partials together. Right now only takes into account the header and footer partials.
+    /*
+    function sendJS(){
+
+    }
+    */
+
+    // Sends requested HTML files.
     function sendPage(view) {
         let count = 0;
         let partialDir = './partials/';
         let partialFiles = ['header.html', 'footer.html'];
         let pageParts = [];
 
+        // Puts the view and the partials together. Right now only takes into account the header and footer partials.
         function makePage(i) {
             fs.readFile(partialDir + partialFiles[i], (err, data) => {
                 pageParts[i] = data.toString();
@@ -38,34 +54,35 @@ var myServer = http.createServer((req, res) => {
         }
     }
 
-    /*
-    function sendCSS(data){
-
+    // Sends requested CSS files.
+    function sendCSS(css) {
+        res.writeHead(200, { 'Content-Type': 'text/css' });
+        res.write(css);
+        res.end();
     }
-    */
 
     // Route the requests.
     switch (req.url) {
         case '/':
-            fetchFile('./views/index.html', sendPage);
+            fetchFile('./views/index.html');
             break;
+
         // TEMP: This is just for testing.
         case '/ls':
             res.writeHead(200, { 'Content-Type': 'text/plain' });
-            getDirItems('.', (result) => res.end(result));
+            getDirItems('.', sendPage);
             break;
+
         // Serve the web page.
         case '/courses':
-            fetchFile('./views/courses.html', sendPage);
+            fetchFile('./views/courses.html');
             break;
+
         // Serve the CSS to go along with the web page. TODO: Make this more dynamic.
         case '/css/main.css':
-            fs.readFile('./css/main.css', (err, data) => {
-                res.writeHead(200, { 'Content-Type': 'text/css' });
-                res.write(data);
-                res.end();
-            });
+            fetchFile('./css/main.css');
             break;
+
         default:
             res.writeHead(404, { 'Content-Type': 'text/plain' });
             res.end('Page not found.');
@@ -78,11 +95,13 @@ var myServer = http.createServer((req, res) => {
 
 // List all the items in a directory.
 function getDirItems(dir, callback) {
-    let body = '';
+    let body = '<ul>';
     fs.readdir(dir, (err, list) => {
-        for (let i = 0; i < list.length; i++)
-            body += list[i] + '\n';
+        for (let i = 0; i < list.length; i++) {
+            body += '<li>' + list[i] + '</li>';
+        }
 
+        body += '</ul>';
         callback(body);
     });
 }
