35336503ff8456a60168daaf3d80551d76a93f35
PJ623 <jardiolin.paolo@gmail.com>
2018-02-07 18:00:22 -0800

    restructured repo, added npm modules, started using handlebars

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var express = require('express');
 var app = express();
 const port = 3000;
 
+var fs = require('fs');
+
 // TODO:
 // Connect DB
 // Complete some part of front end
@@ -14,6 +16,8 @@ const port = 3000;
 var bodyparser = require('body-parser');
 
 // Set the view engine as handlebars.
+var hbs = require('express-handlebars');
+app.engine('handlebars', hbs({extname: 'handlebars', defaultLayout: 'layout', layoutsDir: __dirname + '/views/layouts/'}));
 app.set('view engine', 'handlebars');
 
 // Use body-parser middleware. NOT REALLY USED YET...
@@ -21,7 +25,7 @@ app.use(bodyparser.urlencoded({extended: false}));
 
 // Index
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.render('index', { title: 'HEY'});
 });
 
 // General courses
@@ -37,6 +41,30 @@ app.get('/courses/:courseId', (req, res) => {
 // User profile
 app.get('/user/:userId', (req, res) => {
     res.send('got user ' + req.params.userId + '!');
+});
+
+// Respond with CSS
+app.get('/css/:cssfile', (req, res) => {
+    fs.readFile('./' + req.url, (err, data) => {
+        res.writeHead(200, {"Content-Type": 'text/css'});
+        res.write(data);
+        res.end();
+    });
+});
+
+// Respond with JS
+app.get('/js/:jsfile', (req, res) => {
+    fs.readFile('./' + req.url, (err, data) => {
+        res.writeHead(200, {"Content-Type": 'text/js'});
+        res.write(data);
+        res.end()
+    });
+});
+
+// See whatever requests are leftover to be handled.
+app.get(/.*/, (req, res) => {
+    console.log(req.url);
+    res.end();
 });
 
 // Start server
