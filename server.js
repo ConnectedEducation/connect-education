5c655d696d4b3de3edd9ab852e6ecac346164f4e
nicosantarin <nicosantarin@hotmail.com>
2018-04-08 15:45:01 -0700

    Added login page
    hotfix to remove default css from login page

--- server.js
+++ server.js
@@ -327,7 +327,7 @@ app.get(/.*/, (req, res) => {
 // Socket.io stuff
 io.on('connection', (socket) => {
     console.log('a user connected');
-    
+
     socket.on('disconnect', () => {
         console.log('user disconnected');
     });
